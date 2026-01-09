b72491da4677f5d1dd3d63f16a819c902a282fe1
Nouf Almutairi <noufn8988@gmail.com>
2026-01-10 01:56:57 +0300

    Update facade.py

--- part2/hbnb/app/services/facade.py
+++ part2/hbnb/app/services/facade.py
@@ -1,10 +1,15 @@
+
+
 from app.persistence.repository import InMemoryRepository
 from app.models.user import User
+from app.models.place import Place
 
 class HBnBFacade:
     def __init__(self):
         self.user_repo = InMemoryRepository()
+        self.place_repo = InMemoryRepository()
 
+    # Users
     def create_user(self, user_data):
         user = User(**user_data)
         self.user_repo.add(user)
@@ -25,3 +30,33 @@ class HBnBFacade:
             return None
         user.update(data)
         return user
+
+    # Places
+    def create_place(self, place_data):
+        place = Place(**place_data)
+        valid, error = place.validate()
+        if not valid:
+            return None, error
+        self.place_repo.add(place)
+        return place, None
+
+    def get_place(self, place_id):
+        return self.place_repo.get(place_id)
+
+    def get_all_places(self):
+        return self.place_repo.get_all()
+
+    def update_place(self, place_id, data):
+        place = self.get_place(place_id)
+        if not place:
+            return None, "Place not found"
+
+        for key, value in data.items():
+            if hasattr(place, key):
+                setattr(place, key, value)
+
+        valid, error = place.validate()
+        if not valid:
+            return None, error
+
+        return place, None
