79282e8533ce64746e2f70ab527c2c19730a68a2
Nouf Almutairi <noufn8988@gmail.com>
2026-01-09 22:31:19 +0300

    Update __init__.py

--- part2/hbnb/app/api/__init__.py
+++ part2/hbnb/app/api/__init__.py
@@ -1,6 +1,6 @@
 from flask_restx import Api
 from app.api.v1.amenities import api as amenities_ns
-
+from app.api.v1.places import api as places_ns
 api = Api(
     title="HBnB API",
     version="1.0",
@@ -8,3 +8,4 @@ api = Api(
 )
 
 api.add_namespace(amenities_ns, path="/api/v1/amenities")
+api.add_namespace(places_ns, path="/api/v1/places")
