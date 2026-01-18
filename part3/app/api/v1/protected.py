74e16fd430169f64a9353696aecce3ba1cba8984
saraum1 <saaraaaallls1@gmail.com>
2026-01-18 15:38:59 +0300

    Create protected.py

---
+++ part3/app/api/v1/protected.py
@@ -0,0 +1,18 @@
+from flask_restx import Namespace, Resource
+from flask_jwt_extended import jwt_required, get_jwt_identity, get_jwt
+
+api = Namespace("protected", description="Protected operations")
+
+
+@api.route("/protected")
+class ProtectedResource(Resource):
+    @jwt_required()
+    def get(self):
+        user_id = get_jwt_identity()
+        claims = get_jwt()
+        is_admin = claims.get("is_admin", False)
+
+        return {
+            "message": f"Hello user {user_id}",
+            "is_admin": is_admin
+        }, 200
