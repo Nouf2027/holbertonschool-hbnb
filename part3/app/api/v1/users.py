068b07aa6e54e174c0e833481051fb34b5ced37f
Nouf Almutairi <noufn8988@gmail.com>
2026-01-28 14:06:22 +0300

    Update users.py Task 3

--- part3/app/api/v1/users.py
+++ part3/app/api/v1/users.py
@@ -1,4 +1,5 @@
 from flask_restx import Namespace, Resource, fields
+from flask_jwt_extended import jwt_required, get_jwt, get_jwt_identity
 from app.services import facade
 
 api = Namespace("users", description="User operations")
@@ -37,6 +38,7 @@ user_created_out = api.model("UserCreatedOut", {
 class UserList(Resource):
     @api.marshal_list_with(user_out, code=200)
     def get(self):
+          """Get list of users"""
         users = facade.get_all_users()
         return [u.to_dict() for u in users], 200
 
@@ -55,13 +57,42 @@ class UserList(Resource):
 class UserResource(Resource):
     @api.marshal_with(user_out, code=200)
     def get(self, user_id):
+           """Get user by ID"""
         user = facade.get_user(user_id)
         if not user:
             api.abort(404, "User not found")
         return user.to_dict(), 200
 
-    @api.expect(user_update, validate=True)
-    @api.marshal_with(user_out, code=200)
+        return {
+            'id': user.id,
+            'first_name': user.first_name,
+            'last_name': user.last_name,
+            'email': user.email
+        }, 200
+
+        @jwt_required()
+@api.expect(user_update, validate=True)
+@api.marshal_with(user_out, code=200)
+   @api.response(403, 'Unauthorized action')
+    @api.response(400, 'Invalid update')
+
+def put(self, user_id):
+  """
+        Update user:
+        - Admin: can update any user (including email/password)
+        - User: can update only himself (no email/password)
+        """
+        user = facade.get_user(user_id)
+        if not user:
+            return {'error': 'User not found'}, 404
+
+        return {
+            'id': user.id,
+            'first_name': user.first_name,
+            'last_name': user.last_name,
+            'email': user.email
+        }, 200
+  
     def put(self, user_id):
         user, error = facade.update_user(user_id, api.payload)
         if error:
@@ -69,3 +100,14 @@ class UserResource(Resource):
                 api.abort(404, error)
             api.abort(400, error)
         return user.to_dict(), 200
+
+ # Admin password update
+        if is_admin and 'password' in data:
+            user.hash_password(data['password'])
+
+        return {
+            'id': user.id,
+            'first_name': user.first_name,
+            'last_name': user.last_name,
+            'email': user.email
+        }, 200
