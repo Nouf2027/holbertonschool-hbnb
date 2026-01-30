8734df10e4cb88449575fe93f8876f4f416d9c49
Nouf Almutairi <noufn8988@gmail.com>
2026-01-30 11:21:53 +0300

    Update __init__.py

--- part3/app/__init__.py
+++ part3/app/__init__.py
@@ -2,7 +2,9 @@ from flask import Flask
 from flask_bcrypt import Bcrypt
 from flask_jwt_extended import JWTManager
 from app.api.v1 import init_api
+from flask_sqlalchemy import SQLAlchemy
 
+db = SQLAlchemy()
 bcrypt = Bcrypt()
 jwt = JWTManager()
 
