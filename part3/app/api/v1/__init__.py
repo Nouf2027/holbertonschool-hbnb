5c66fa497e02fa052c18a236d121307c7bc11b99
Nouf Almutairi <noufn8988@gmail.com>
2026-01-30 10:58:41 +0300

    Update __init__.py

--- part3/app/api/v1/__init__.py
+++ part3/app/api/v1/__init__.py
@@ -4,7 +4,7 @@ from app.api.v1.users import api as users_api
 from app.api.v1.auth import api as auth_api
 from app.api.v1.protected import api as protected_api
 from api.v1.views.amenities import api as amenities_ns
-
+from app.api.v1.places import api as places_api
 
 
 
@@ -14,5 +14,6 @@ def init_api(app):
     api.add_namespace(users_api, path="/api/v1/users")
     api.add_namespace(auth_api, path="/api/v1/auth")
     api.add_namespace(protected_api, path="/api/v1")
-api.add_namespace(amenities_ns, path='/amenities')
+api.add_namespace(amenities_ns, path='/api/v1/amenities')
+api.add_namespace(places_api, path='/api/v1/places')
     return api
