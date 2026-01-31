347ca1e7fb33349004625f0d8cb53d5727deee23
roya86 <royaabduallah@gmail.com>
2026-01-31 20:25:24 +0300

    Update files

--- part3/app/models/amenity.py
+++ part3/app/models/amenity.py
@@ -1,7 +1,14 @@
 from app import db
 from app.models.base_model import BaseModel
+from app.models.place_amenity import place_amenity
 
 class Amenity(BaseModel):
     __tablename__ = 'amenities'
 
     name = db.Column(db.String(50), nullable=False, unique=True)
+
+    places = db.relationship(
+        'Place',
+        secondary=place_amenity,
+        back_populates='amenities'
+    )
--- part3/app/models/place.py
+++ part3/app/models/place.py
@@ -1,5 +1,6 @@
 from app import db
 from app.models.base_model import BaseModel
+from app.models.place_amenity import place_amenity
 
 class Place(BaseModel):
     __tablename__ = 'places'
@@ -10,7 +11,29 @@ class Place(BaseModel):
     latitude = db.Column(db.Float)
     longitude = db.Column(db.Float)
 
+    owner_id = db.Column(
+        db.String(36),
+        db.ForeignKey('users.id', ondelete='CASCADE'),
+        nullable=False
+    )
 
-    owner_id = db.Column(db.String(36), db.ForeignKey('users.id'), nullable=False)
-    
+    # User (1) -> (*) Place
+    owner = db.relationship(
+        'User',
+        back_populates='places'
+    )
 
+    # Place (1) -> (*) Review
+    reviews = db.relationship(
+        'Review',
+        back_populates='place',
+        cascade='all, delete-orphan',
+        passive_deletes=True
+    )
+
+    # Place (*) <-> (*) Amenity
+    amenities = db.relationship(
+        'Amenity',
+        secondary=place_amenity,
+        back_populates='places'
+    )
